80a1ab884178a3c8c998c181be43a42e85570033
dimple365 <dim8ple05@gmail.com>
2025-08-29 02:18:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ col = ['MedInc', 'HouseAge', 'AveRooms', 'AveBedrms', 'Population', 'AveOccup']
 
 st.title('California Housing Price Prediction')
 
-st.image('https://www.propertyreporter.co.uk/images/660x350/16402-shutterstock_538341163.jpg')
+st.image('https://lh3.googleusercontent.com/gg-dl/AJfQ9KQhdo7p6FpWwnL73MU-z2eOifOkIoYMMb8uUwbvVJ8q1MMvTmpnYdaNaeXyiASochBhYsQfPYOCkbq1EF0-PdWe5b0z0F-V4Z-CYtjp-LvPk9qzeROKFhLYoe607MJqAI1zZSHp4-GTKFLwJHfF9aBoQHKwM_SU66SKals4e0-uGf-6WA=s1024')
 
 
 
